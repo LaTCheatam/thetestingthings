026901748c6341778f9c1513f3af28748d310a1a
adambailey <arb5433@gmail.com>
2021-04-12 18:44:59 -0400

    “refactored”

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,17 +1,35 @@
 def parse(value):
-  if value == "I":
-    return 1
-  elif value == "II":
-    return 2
-  elif value == 'III':
-    return 3
-  elif value == 'IV':
+    # IV edge case
+
+   # v = 5, i = 1, add them
+
+
+  if value[0] == 'I' and value[-1] == 'V':
     return 4
-  elif value == 'V':
-    return 5
-  elif value == 'VI':
-    return 6
-  elif value == 'VII':
-    return 7
-  elif value == 'VIII':
-    return 8
+  else:
+    answer = 0
+    for digit in value:
+      if digit == 'I':
+        answer += 1
+      if digit == 'V':
+        answer += 5
+    return answer
+
+
+  # if value == "I":
+  #   return 1
+  # elif value == "II":
+  #   return 2
+  # elif value == 'III':
+  #   return 3
+    
+  # elif value == 'IV':
+  #   return 4
+  # elif value == 'V':
+  #   return 5
+  # elif value == 'VI':
+  #   return 6
+  # elif value == 'VII':
+  #   return 7
+  # elif value == 'VIII':
+  #   return 8
